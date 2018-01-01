fe544bbf1fb18f1fd6331306778ddf47431b11f8
Navsingh <navsingh191@gmail.com>
2018-01-01 12:58:35 -0500

    Basic Animation Implemented Animated Circle from rounded rectangle code is here

--- app/src/main/java/com/example/navjot/appwidgetnotifiaction/ViewCircleAnimation.java
+++ app/src/main/java/com/example/navjot/appwidgetnotifiaction/ViewCircleAnimation.java
@@ -36,52 +36,15 @@ class ViewCircleAnimation extends View {
         init();
 
     }
-
-    /**
-     * Constructor that is called when inflating a view from XML. This is called
-     * when a view is being constructed from an XML file, supplying attributes
-     * that were specified in the XML file. This version uses a default style of
-     * 0, so the only attribute values applied are those in the Context's Theme
-     * and the given AttributeSet.
-     * <p>
-     * <p>
-     * The method onFinishInflate() will be called after all children have been
-     * added.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     */
     public ViewCircleAnimation(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs,0);
         init();
     }
-
-    /**
-     * Perform inflation from XML and apply a class-specific base style from a
-     * theme attribute. This constructor of View allows subclasses to use their
-     * own base style when they are inflating. For example, a Button class's
-     * constructor would call this version of the super class constructor and
-     * supply <code>R.attr.buttonStyle</code> for <var>defStyleAttr</var>; this
-     * allows the theme's button style to modify all of the base view attributes
-     * (in particular its background) as well as the Button class's attributes.
-     *
-     * @param context      The Context the view is running in, through which it can
-     *                     access the current theme, resources, etc.
-     * @param attrs        The attributes of the XML tag that is inflating the view.
-     * @param defStyleAttr An attribute in the current theme that contains a
-     *                     reference to a style resource that supplies default values for
-     *                     the view. Can be 0 to not look for defaults.
-     */
     public ViewCircleAnimation(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
 
-    /**
-     * Implement this to do your drawing.
-     *
-     */
     protected  void init()
     {
         paint  = new Paint();
@@ -90,8 +53,8 @@ class ViewCircleAnimation extends View {
         PropertyValuesHolder  propertyRotate = PropertyValuesHolder.ofInt(PROPERTY_ROTATE,0,360);
         // ValueAnimator valueAnimator = ValueAnimator.ofInt(5,150);
         ValueAnimator valueAnimator = new ValueAnimator();
-        //valueAnimator.setValues(propertyRadius,propertyRotate);
-        valueAnimator.setInterpolator(new BounceInterpolator());
+        valueAnimator.setValues(propertyRadius,propertyRotate);
+        //valueAnimator.setInterpolator(new BounceInterpolator());
         valueAnimator.setInterpolator(new CycleInterpolator(10));
         valueAnimator.setDuration(2000);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -99,6 +62,7 @@ class ViewCircleAnimation extends View {
             public void onAnimationUpdate(ValueAnimator animation) {
                 radius = (int)animation.getAnimatedValue(PROPERTY_RADIUS);
                 rotate = (int)animation.getAnimatedValue(PROPERTY_ROTATE);
+                //Toast.makeText(getContext(),"ok",Toast.LENGTH_LONG).show();
                 invalidate();
             }
         });
@@ -116,6 +80,7 @@ class ViewCircleAnimation extends View {
         int rightBotX = viewWidth+150;
         int rightBotY = viewHeight+150;
         canvas.rotate(rotate,viewWidth,viewHeight);
+
         canvas.drawRoundRect(leftTopX,leftTopY,rightBotX,rightBotY,radius,radius,paint);
 
     }
